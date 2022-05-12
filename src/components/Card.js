0dd945ee96fec0ac9638ae576035d0b676d0339b
kubilay <kubidik27@gmail.com>
2022-05-13 02:45:26 +0300

    question mark icon created & flip animation done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,19 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, {useState} from "react";
 import style from "../styles/card.module.scss";
+import QuestionMark from "../constants/icons/QuestionMark";
 
 
 // eslint-disable-next-line react/prop-types
 function Card({ id, image }) {
+	const [isFlipped, setIsFlipped] = useState(false);
+
 	return (
-		<div className={style.card}>
+		<div className={`${style.card} ${isFlipped ? style.flipped : ""}`}  onClick={()=>setIsFlipped(!isFlipped)}>
 			<img src={image} alt={id} />
+			<div className={style.default}>
+				<QuestionMark />
+			</div>
 		</div>
 	);
 }
