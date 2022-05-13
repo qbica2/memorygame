9f7c2d1b442ef9cf9cab70e5259a27cde64c1384
kubilay <kubidik27@gmail.com>
2022-05-13 21:42:20 +0300

    panel component designed

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -5,7 +5,33 @@ function Panel() {
 	return (
 		<div className={style.panel}>
 			<div className={style.content}>
-				<h1>Memory Game</h1>
+				<div className={style.gameplay}>
+					<div className={style.title}>How to play?</div>
+					<span>
+						Click on the cards to see if you can find the matching pairs.
+					</span>
+					<span>
+						The game is over when all the cards are matched or time is up.
+					</span>
+				</div>
+				<div className={style.difficulty}>
+					<div className={style.title}>Select Difficulty</div>
+					<div className={style.buttons}>
+						<button className={style.notSelected}>Easy</button>
+						<button className={style.notSelected}>Medium</button>
+						<button>Hard</button>
+					</div>
+				</div>
+				<div className={style.mode}>
+					<div className={style.title}>Select Game Mode</div>
+					<div className={style.buttons}>
+						<button className={style.notSelected}>Brands</button>
+						<button >Teams</button>
+					</div>
+				</div>
+				
+				<button className={style.start}>Start</button>
+				
 			</div>
 		</div>
 	);
